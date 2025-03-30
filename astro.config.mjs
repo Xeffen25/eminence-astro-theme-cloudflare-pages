dda70a296de607e9263d26e0134d768d016a5816
xeffen25 <xeffen25@xeffen25.com>
2025-03-30 22:52:32 +0000

    Feat: Localization

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	i18n: {
+		defaultLocale: "es",
+		locales: ["es"],
+	},
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
 		},
 	}),
-
 	vite: {
 		plugins: [tailwindcss()],
 	},
